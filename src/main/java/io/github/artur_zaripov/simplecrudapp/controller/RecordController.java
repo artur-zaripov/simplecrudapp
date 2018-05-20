0253a435248bf8cf36bce7ec2909c2069df3289a
Artur Zaripov <artur.v.zaripov@gmail.com>
2018-05-21 00:05:27 +0300

    added DTO for records API

--- src/main/java/io/github/artur_zaripov/simplecrudapp/controller/RecordController.java
+++ src/main/java/io/github/artur_zaripov/simplecrudapp/controller/RecordController.java
@@ -1,9 +1,11 @@
 package io.github.artur_zaripov.simplecrudapp.controller;
 
+import io.github.artur_zaripov.simplecrudapp.dto.RecordDTO;
 import io.github.artur_zaripov.simplecrudapp.exception.RecordNotFoundException;
 import io.github.artur_zaripov.simplecrudapp.model.ApiResponse;
 import io.github.artur_zaripov.simplecrudapp.model.Record;
 import io.github.artur_zaripov.simplecrudapp.service.RecordService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "records")
@@ -21,39 +24,49 @@ public class RecordController {
 
     private final RecordService recordService;
 
+    private final ModelMapper modelMapper = new ModelMapper();
+
     @Autowired
     public RecordController(RecordService recordService) {
         this.recordService = recordService;
     }
 
     @GetMapping(value = "/id/{recordId}")
-    public Record getRecord(@PathVariable("recordId") Integer recordId) {
-        return recordService.getRecordById(recordId)
-                .orElseThrow(() -> new RecordNotFoundException(recordId));
+    public RecordDTO getRecord(@PathVariable("recordId") Integer recordId) {
+        return modelMapper.map(
+                recordService.getRecordById(recordId).orElseThrow(() -> new RecordNotFoundException(recordId)),
+                RecordDTO.class
+        );
     }
 
     @GetMapping(value = "/name/{name}")
-    public List<Record> getRecords(@PathVariable("name") String name) {
-        return recordService.getRecordsByName(name);
+    public List<RecordDTO> getRecords(@PathVariable("name") String name) {
+        return recordService.getRecordsByName(name)
+                .stream()
+                .map(r -> modelMapper.map(r, RecordDTO.class))
+                .collect(Collectors.toList());
     }
 
     @GetMapping(value = "/from/{from}/to/{to}")
-    public List<Record> getRecords(
+    public List<RecordDTO> getRecords(
             @PathVariable("from") @DateTimeFormat(pattern = DATE_PATTERN) LocalDateTime from,
             @PathVariable("to") @DateTimeFormat(pattern = DATE_PATTERN) LocalDateTime to
     ) {
-        return recordService.getRecordsByDate(from, to);
+        return recordService.getRecordsByDate(from, to)
+                .stream()
+                .map(r -> modelMapper.map(r, RecordDTO.class))
+                .collect(Collectors.toList());
     }
 
     @PostMapping(value = "/")
-    public ResponseEntity<ApiResponse> saveRecord(@RequestBody Record record) {
-        recordService.saveRecord(record);
+    public ResponseEntity<ApiResponse> saveRecord(@RequestBody RecordDTO record) {
+        recordService.saveRecord(modelMapper.map(record, Record.class));
         return new ResponseEntity<>(new ApiResponse("Record successfully saved"), HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/")
-    public ResponseEntity<ApiResponse> updateRecord(@RequestBody Record record) {
-        recordService.updateRecord(record);
+    public ResponseEntity<ApiResponse> updateRecord(@RequestBody RecordDTO record) {
+        recordService.updateRecord(modelMapper.map(record, Record.class));
         return new ResponseEntity<>(new ApiResponse("Record successfully updated"), HttpStatus.OK);
     }
 
---
+++ src/main/java/io/github/artur_zaripov/simplecrudapp/dto/RecordDTO.java
@@ -0,0 +1,49 @@
+package io.github.artur_zaripov.simplecrudapp.dto;
+
+import java.time.LocalDateTime;
+
+public class RecordDTO {
+
+    private Integer id;
+
+    private String name;
+
+    private LocalDateTime creationDate;
+
+    private String description;
+
+    public RecordDTO() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
